22e15a6d154940814175044d4c5322ee5dd0f3c8
Jiseok CHOI <jiseok.dev@gmail.com>
2022-05-02 21:01:14 +0900

    Add `PATH` variable in common namespace

--- common/src/index.ts
+++ common/src/index.ts
@@ -2,6 +2,7 @@ export type Method = "GET" | "POST" | "DELETE" | "PATCH" | "PUT" | "OPTIONS";
 
 export namespace Example {
   export const METHOD: Method = "GET";
+  export const PATH: string = "/example";
 
   export interface Request {
     querystring: {
