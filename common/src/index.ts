bc8c02d4b9b5208f49cfc32f20818c239da4422c
Jiseok CHOI <jiseok.dev@gmail.com>
2022-05-02 21:05:39 +0900

    Implement `Health` skeleton route

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,5 +1,16 @@
 export type Method = "GET" | "POST" | "DELETE" | "PATCH" | "PUT" | "OPTIONS";
 
+export namespace Health {
+  export const METHOD: Method = "GET";
+  export const PATH: string = "/health";
+
+  export interface Request {}
+
+  export interface Response {
+    success: boolean;
+  }
+}
+
 export namespace Example {
   export const METHOD: Method = "GET";
   export const PATH: string = "/example";
