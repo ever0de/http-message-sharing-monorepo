6bb11b0ab0a5b9a16dc52ad9a0e2f890a6e08175
Jiseok CHOI <jiseok.dev@gmail.com>
2022-05-08 23:00:56 +0900

    Enable 'tabWidth' to 4

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,27 +1,27 @@
 export type Method = "GET" | "POST" | "DELETE" | "PATCH" | "PUT" | "OPTIONS";
 
 export namespace Health {
-  export const METHOD: Method = "GET";
-  export const PATH: string = "/health";
+    export const METHOD: Method = "GET";
+    export const PATH: string = "/health";
 
-  export interface Request {}
+    export interface Request {}
 
-  export interface Response {
-    success: boolean;
-  }
+    export interface Response {
+        success: boolean;
+    }
 }
 
 export namespace Example {
-  export const METHOD: Method = "GET";
-  export const PATH: string = "/example";
+    export const METHOD: Method = "GET";
+    export const PATH: string = "/example";
 
-  export interface Request {
-    querystring: {
-      name: string;
-    };
-  }
+    export interface Request {
+        querystring: {
+            name: string;
+        };
+    }
 
-  export interface Response {
-    name: string;
-  }
+    export interface Response {
+        name: string;
+    }
 }
