63077aa6103504e553213c4fa21d1c5de3dce3ad
Jiseok CHOI <jiseok.dev@gmail.com>
2022-05-10 19:19:55 +0900

    Replace prettier extension `yaml` to `js`

---
+++ .prettierrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    bracketSpacing: true,
+    bracketSameLine: false,
+    singleQuote: false,
+    trailingComma: "all",
+    semi: true,
+    useTabs: false,
+    tabWidth: 4,
+    endOfLine: "lf",
+
+    plugins: [require("@trivago/prettier-plugin-sort-imports")],
+    importOrder: ["<THIRD_PARTY_MODULES>", "@/(.*)$", "^[./](.*)$"],
+    importOrderSeparation: true,
+    importOrderSortSpecifiers: true,
+
+    overrides: [
+        {
+            files: ["*.json", "*.yaml", "&.yml"],
+            options: {
+                tabWidth: 2,
+            },
+        },
+    ],
+};
