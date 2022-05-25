037dcd54c1d2e6a0ba87e2ebd62c47727b2ecaa3
Jiseok CHOI <jiseok.dev@gmail.com>
2022-05-25 17:01:24 +0900

    Change to use Fastify Adapter in `e2e` test,
    Reference: <https://stackoverflow.com/questions/70041367/end-to-end-testing-nestjs-with-fastify-nestjs-platform-express-package-is-mi>

--- api/test/app.e2e-spec.ts
+++ api/test/app.e2e-spec.ts
@@ -1,4 +1,8 @@
 import { INestApplication } from "@nestjs/common";
+import {
+    FastifyAdapter,
+    NestFastifyApplication,
+} from "@nestjs/platform-fastify";
 import { Test } from "@nestjs/testing";
 import request from "supertest";
 
@@ -12,14 +16,24 @@ describe("AppController (e2e)", () => {
             imports: [AppModule],
         }).compile();
 
-        app = moduleFixture.createNestApplication();
+        app = moduleFixture.createNestApplication<NestFastifyApplication>(
+            new FastifyAdapter(),
+        );
         await app.init();
+        await app.getHttpAdapter().getInstance().ready();
     });
 
-    it("/ (GET)", () => {
+    it("[GET] /", () => {
         return request(app.getHttpServer())
             .get("/")
             .expect(200)
             .expect("Hello World!");
+    });
+
+    it("[GET] /health", () => {
+        return request(app.getHttpServer())
+            .get("/health")
+            .expect(200)
+            .expect(JSON.stringify({ success: true }));
     });
 });
