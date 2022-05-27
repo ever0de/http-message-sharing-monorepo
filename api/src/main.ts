d1beec84a357bbb4ad32f9177dfde7dc85a66d1b
Jiseok CHOI <jiseok.dev@gmail.com>
2022-05-27 17:18:20 +0900

    Export env `PORT`

--- api/src/main.ts
+++ api/src/main.ts
@@ -6,11 +6,13 @@ import {
 
 import { AppModule } from "./app.module";
 
+const PORT = process.env.PORT || "3000";
+
 async function bootstrap() {
     const app = await NestFactory.create<NestFastifyApplication>(
         AppModule,
         new FastifyAdapter(),
     );
-    await app.listen(3000);
+    await app.listen(PORT);
 }
 bootstrap();
