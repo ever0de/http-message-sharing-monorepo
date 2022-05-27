c542dfe2ee54db9dda8f3ab6b680d190c60fe7da
seung suk lee <steve8060914@gmail.com>
2022-05-27 17:34:09 +0900

    Setup simple swagger (#11)
    * feat: swagger installl
    
    * delete: duplication

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,6 +3,7 @@ import {
     FastifyAdapter,
     NestFastifyApplication,
 } from "@nestjs/platform-fastify";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 import { AppModule } from "./app.module";
 
@@ -13,6 +14,16 @@ async function bootstrap() {
         AppModule,
         new FastifyAdapter(),
     );
+
+    const config = new DocumentBuilder()
+        .setTitle("Cats example")
+        .setDescription("The cats API description")
+        .setVersion("1.0")
+        .addTag("cats")
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup("api", app, document);
+
     await app.listen(PORT);
 }
 bootstrap();
