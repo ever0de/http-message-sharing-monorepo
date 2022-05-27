052e35746d1fa2d7374ac33597182bdd373ce5e1
Jiseok CHOI <jiseok.dev@gmail.com>
2022-05-27 16:42:36 +0900

    Setup monorepo
    Co-authored-by: seung suk lee <steve806@users.noreply.github.com>

--- api/src/health/health.controller.ts
+++ api/src/health/health.controller.ts
@@ -1,10 +1,10 @@
-// import HttpMessage from "@ever0de/http-message";
+import * as HttpMessage from "@ever0de/http-message";
 import { Controller, Get } from "@nestjs/common";
 
 @Controller("health")
 export class HealthController {
     @Get()
-    health(): { success: boolean } {
+    health(): HttpMessage.Root.Health.Response {
         return { success: true };
     }
 }
