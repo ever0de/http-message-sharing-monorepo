7d14ed1290bb8a590358731b55202b27a7ac7f12
steve <steve8060914@gmail.com>
2022-05-27 17:28:25 +0900

    feat: swagger installl

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,6 +3,7 @@ import {
     FastifyAdapter,
     NestFastifyApplication,
 } from "@nestjs/platform-fastify";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 import { AppModule } from "./app.module";
 
@@ -11,6 +12,16 @@ async function bootstrap() {
         AppModule,
         new FastifyAdapter(),
     );
+
+    const config = new DocumentBuilder()
+        .setTitle("Cats example")
+        .setDescription("The cats API description")
+        .setVersion("1.0")
+        .addTag("cats")
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup("api", app, document);
+
     await app.listen(3000);
 }
 bootstrap();
