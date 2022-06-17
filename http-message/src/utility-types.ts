2084544f434db7d8432c056441a7707a8479e19e
Jiseok CHOI <jiseok.dev@gmail.com>
2022-06-17 13:31:54 +0900

    Implement `RouteParams` type

--- http-message/src/utility-types.ts
+++ http-message/src/utility-types.ts
@@ -9,3 +9,11 @@ export type Range<F extends number, T extends number> = Exclude<
     Enumerate<T>,
     Enumerate<F>
 >;
+
+export type RouteParams<T extends string> = string extends T
+    ? Record<string, string>
+    : T extends `${infer _}:${infer Param}/${infer Rest}`
+    ? { [k in Param | keyof RouteParams<Rest>]: string }
+    : T extends `${infer _}:${infer Param}`
+    ? { [k in Param]: string }
+    : null;
